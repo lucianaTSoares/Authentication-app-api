9a5da9b5b3dac7afbc4c98c7f499256bdb641034
lucianaTSoares <lucianat.s@hotmail.com>
2023-08-19 18:50:35 -0300

    doc: add documentation on auth.service methods

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UserService } from '../user/user.service';
 import { User } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
+import { UserService } from '../user/user.service';
 import { AuthPayload } from './payload.model';
 
 @Injectable()
@@ -12,6 +12,13 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
 
+  /**
+   * Validates a user's email and password.
+   *
+   * @param {string} email - The user's email.
+   * @param {string} password - The user's password.
+   * @return {Promise<User>} - The validated user object.
+   */
   async validateUser(email: string, password: string): Promise<User> {
     const user = await this.userService.findOneByEmail(email);
     const isPasswordValid = await bcrypt.compare(
@@ -29,6 +36,12 @@ export class AuthService {
     };
   }
 
+  /**
+   * Signs in a user and returns an access token.
+   *
+   * @param {User} user - The user object containing the user's ID and email.
+   * @return {Promise<{ access_token: string }>} - A promise that resolves to an object containing the access token.
+   */
   async signIn(user: User): Promise<{ access_token: string }> {
     const payload: AuthPayload = { sub: user.id, email: user.email };
 
